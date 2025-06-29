4ca152c76ae98fe6c555f47821d7e075cafc8167
Amelia <nawrotamelia3@gmail.com>
2025-06-29 17:46:51 +0200

    plots

--- plot_2D.m
+++ plot_2D.m
@@ -2,16 +2,16 @@ clear variables;
 clc;
 close all;
 
-x=rand(1,1000);
+x = rand(1, 1000);
 figure;
-hist(x);
+hist(x);                         % Histogram of 1000 uniformly distributed random numbers
 saveas(gcf, 'histogram.png');
 
-X=[1 2 3 4];
+X = [1 2 3 4];
 figure;
-pie(X);
+pie(X);                          % 2D pie chart of values in vector X
 saveas(gcf, 'pie_x.png');
 
 figure;
-pie3(X);
+pie3(X);                         % 3D pie chart of values in vector X
 saveas(gcf, 'pie3_x.png');
--- simple_plot.m
+++ simple_plot.m
@@ -4,5 +4,13 @@ close all;
 
 x=linspace(-pi,pi,2);
 y=sin(x);
+
 figure;
 plot(x,y);
+
+figure;
+bar(x,y);
+
+figure;
+
+
